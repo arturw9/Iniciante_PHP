f414ddb4f95f66a80892877df36bb1b6115d75b8
FidelisArtur <arturfidelis@pactosolucoes.com.br>
2022-04-30 00:10:03 -0300

    Conceitos basicos iniciantes PHP

---
+++ decisoes.php
@@ -0,0 +1,15 @@
+<?php
+
+$idade = 20;
+
+echo "Você só pode entrar se for maior que 18 anos".PHP_EOL;
+
+if ($idade >= 18) {
+    echo "Você tem $idade anos.".PHP_EOL;
+    echo "Pode entrar";
+} else {
+    echo "Você só tem $idade anos. Você não pode entrar";
+}
+
+echo PHP_EOL;
+echo "Adeus!";
---
+++ strings.php
@@ -0,0 +1,4 @@
+<?php
+
+ $idade = 25;
+ echo "Olá mundo! \n Minha idade é $idade anos";
