961b8261fb4aab208bd732178dbe33864a353e94
FidelisArtur <arturfidelis@pactosolucoes.com.br>
2022-04-30 10:41:03 -0300

    Conceitos avançado iniciante PHP (Funcoes) sacar/depositar/verifacoes

--- avancado/banco.php
+++ avancado/banco.php
@@ -1,8 +1,27 @@
 <?php
 
+function sacar($conta, $valorASacar)
+{
+    if($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não pode sacar");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+    return $conta;
+}
+
 function exibeMensagem($mensagem)
 {
     echo  $mensagem.PHP_EOL;
+}
+
+function depositar(array $conta, float $valorADepositar): array {
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depositos precisam ser positivos");
+    }
+    return $conta;
 }
 
 $contasCorrentes = [
@@ -19,11 +38,11 @@ $contasCorrentes = [
         'saldo' => 3000
     ],
 ];
-if(5000 > $contasCorrentes[123455789910]['saldo']) {
-    exibeMensagem("Você não pode sacar");
-} else {
-    $contasCorrentes[123455789910]['saldo'] -= 5000;
-}
+
+$contasCorrentes[123455789910] = sacar($contasCorrentes[123455789910], 500);
+$contasCorrentes[123456789819] = sacar($contasCorrentes[123456789819], 500);
+$contasCorrentes[123456789914] = depositar($contasCorrentes[123456789914], 100);
+
 
 foreach ($contasCorrentes as $cpf => $conta) {
     exibeMensagem($cpf. " ".$conta['titular']. " ".$conta['saldo']);
